559709f4206066550209ab04295ae4fd07f9b496
Sungguk Lim <limasdf@gmail.com>
2015-04-30 18:38:21 +0900

    WIP

--- bin/main.dart
+++ bin/main.dart
@@ -8,6 +8,8 @@ import 'package:xml/xml.dart';
 Map<String, String> LANGUAGE_CODE_MAP = 
 // HERE
   const {
+    "af-ZA": "af",
+    "am-ET": "am",
     "ar-SA": "ar",
     "as-IN": "as",
     "bg-BG": "bg",
@@ -17,10 +19,10 @@ Map<String, String> LANGUAGE_CODE_MAP =
     "da-DK": "da",
     "de-DE": "de",
     "el-GR": "el",
-    "en-CN": "en",
     "en-GB": "en-GB",
-    "en-US": "en",
-    "es-CO": "es",
+    "en-CN": "remove",
+    "en-US": "remove",
+    "es-CO": "es-419",
     "es-ES": "es",
     "et-EE": "et",
     "fa-IR": "fa",
@@ -29,7 +31,8 @@ Map<String, String> LANGUAGE_CODE_MAP =
     "fr-FR": "fr",
     "ga-IE": "ga",
     "gu-IN": "gu",
-    "he-IL": "he",
+    "ha-Latn-NG": "ha",
+    "he-IL": "iw",
     "hi-IN": "hi",
     "hr-HR": "hr",
     "hu-HU": "hu",
@@ -47,12 +50,13 @@ Map<String, String> LANGUAGE_CODE_MAP =
     "mn-Cyrl-MN": "mn",
     "mr-IN": "mr",
     "ms-MY": "ms",
-    "nb-NO": "nb",
+    "nb-NO": "no",
     "nl-NL": "nl",
     "pa-IN": "pa",
     "pl-PL": "pl",
     "pt-BR": "pt-BR",
     "pt-PT": "pt-PT",
+    "or-IN": "or",
     "ro-RO": "ro",
     "ru-RU": "ru",
     "sk-SK": "sk",
@@ -66,7 +70,7 @@ Map<String, String> LANGUAGE_CODE_MAP =
     "tr-TR": "tr",
     "uk-UA": "uk",
     "ur-IN": "ur",
-    "uz-Cyrl-UZ": "uz",
+    "uz-Cyrl-UZ": "remove",
     "uz-Latn-UZ": "uz",
     "vi-VN": "vi",
     "zh-Hans-CN": "zh-CN",
@@ -75,7 +79,7 @@ Map<String, String> LANGUAGE_CODE_MAP =
   };
 
 main(List<String> arguments) {
-  String original = new File('/home/sungguk/program_store/localization-data/inputmgr.xliff').readAsStringSync();
+  String original = new File('/home/sungguk/program_store/localization-data_2/quickinputpicker.xliff').readAsStringSync();
   var document = parse(original);
   var translated_text = "";
   var to_write_text = "";
@@ -98,10 +102,11 @@ main(List<String> arguments) {
              if(z.toString().contains("target-language")) {
               //print("<translation id=\"{0}" + "\">" + translated_text + "</translation>");
                to_write_text = "<translation id=\"8517434622200999683" + "\">" + translated_text + "</translation>";
-               print("<translation id=\"8517434622200999683" + "\">" + translated_text + "</translation>");
+               //print("<translation id=\"8517434622200999683" + "\">" + translated_text + "</translation>");
+               print(z.toString());
                expected_file_name = z.toString().replaceFirst("target-language=\"", "").replaceFirst("\"", "");
                expected_file_name = LANGUAGE_CODE_MAP[expected_file_name];
-               print(expected_file_name);
+               //print(expected_file_name);
                var gen = new File("./ttest/generated_resources_" + expected_file_name + ".xtb");
                gen.writeAsStringSync(to_write_text);
              }
