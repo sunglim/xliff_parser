b564814fa61c6e4aa7777955db5ea46177dabe4a
Sungguk Lim <limasdf@gmail.com>
2015-05-11 23:04:16 +0900

    NOT PROGRAMMED

--- bin/main.dart
+++ bin/main.dart
@@ -87,7 +87,7 @@ main(List<String> arguments) {
   document.descendants.forEach((element) {
     // Exactly 
     //if (element.toString() == arguments[0]) {
-    if (element.toString() == "Input Blocked") {
+    if (element.toString() == "Not Programmed") {
       var preced = element.ancestors;
       for(var i in preced) {
         var parent2 = i.ancestors;
@@ -101,7 +101,7 @@ main(List<String> arguments) {
             for(var z in k.attributes) {
              if(z.toString().contains("target-language")) {
               //print("<translation id=\"{0}" + "\">" + translated_text + "</translation>");
-               to_write_text = "<translation id=\"7666559633534040881" + "\">" + translated_text + "</translation>";
+               to_write_text = "<translation id=\"9163082029466067053" + "\">" + translated_text + "</translation>";
                //print("<translation id=\"8517434622200999683" + "\">" + translated_text + "</translation>");
                print(z.toString());
                expected_file_name = z.toString().replaceFirst("target-language=\"", "").replaceFirst("\"", "");
