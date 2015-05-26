42f1255a257a2c51e26bf66a42fb3235602fd0de
Sungguk Lim <limasdf@gmail.com>
2015-05-26 11:17:25 +0900

    OTA Service

--- bin/main.dart
+++ bin/main.dart
@@ -89,7 +89,7 @@ main(List<String> arguments) {
   document.descendants.forEach((element) {
     // Exactly 
     //if (element.toString() == arguments[0]) {
-    if (element.toString() == "Not Supported Service") {
+    if (element.toString() == "OTA Service") {
       var preced = element.ancestors;
       for(var i in preced) {
         var parent2 = i.ancestors;
@@ -103,7 +103,7 @@ main(List<String> arguments) {
             for(var z in k.attributes) {
              if(z.toString().contains("target-language")) {
               //print("<translation id=\"{0}" + "\">" + translated_text + "</translation>");
-               to_write_text = "<translation id=\"4841945968328826810" + "\">" + translated_text + "</translation>";
+               to_write_text = "<translation id=\"5291275329377435149" + "\">" + translated_text + "</translation>";
                //print("<translation id=\"8517434622200999683" + "\">" + translated_text + "</translation>");
                print(z.toString());
                expected_file_name = z.toString().replaceFirst("target-language=\"", "").replaceFirst("\"", "");
